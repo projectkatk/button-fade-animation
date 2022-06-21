3a94902e2e95d3644ed466ba2205f33f80253ac8
Kate Kim <kay.k.projects@gmail.com>
2022-06-21 12:54:49 +1000

    comments updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,24 +6,28 @@ const Button = () => {
     const [ scrollYValue, setScrollYValue ] = useState(0);
     const facebook = useRef();
     const instagram = useRef();
-  
+
+    //function to handle fade in effect
     const handleFadeIn = () => {
       facebook.current.classList.remove(styles.fadeOutRight);
       instagram.current.classList.remove(styles.fadeOutLeft);
       facebook.current.classList.add(styles.fadeMoveRight);
       instagram.current.classList.add(styles.fadeMoveLeft);
     }
-  
+    
+    // function to handle fade out effect
     const handleFadeOut = () => {
       facebook.current.classList.add(styles.fadeOutRight);
       instagram.current.classList.add(styles.fadeOutLeft);
     }
     
     useEffect(() => {
+      // window on scroll event
       window.addEventListener('scroll', () => {
         setScrollYValue(window.scrollY);
       });
       
+      // depending on the scroll Y position, call handleFadeIn or handleFadeOut function
       if((scrollYValue > window.innerHeight / 7) && scrollYValue < window.innerHeight / 0.7) {
         handleFadeIn();
       } else if ((scrollYValue >= window.innerHeight / 0.7) || (scrollYValue <= window.innerHeight / 7)) {
